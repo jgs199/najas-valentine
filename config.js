08d5215939539db7ad89cd35c764463f47eeebac
jgs199 <iniglensullivan199@gmail.com>
2025-02-11 22:39:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "NAYA",
+    valentineName: "Hai Naya",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://asset.cloudinary.com/dr8bv9l0m/294f64f4eebd62ac479b98c981f1d8ad", // Music streaming URL
+        musicUrl: "https://asset.cloudinary.com/dr8bv9l0m/d803c20ace29bbb5cfe4086de1dde8dd", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
